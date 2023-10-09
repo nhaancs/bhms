888dfedec66968bff22bfcafbf05f58eb9c10152
nhannq2 <nhanpublic@gmail.com>
2023-10-09 20:32:48 +0700

    renamed handlers & removed unused code

--- app/services/api/v1/handlers/usergrp/route.go
+++ app/services/api/v1/handlers/usergrp/route.go
@@ -4,12 +4,10 @@ import (
 	"net/http"
 
 	"github.com/jmoiron/sqlx"
-	"github.com/nhaancs/bhms/app/services/api/v1/mid"
 	"github.com/nhaancs/bhms/business/core/event"
 	"github.com/nhaancs/bhms/business/core/user"
 	"github.com/nhaancs/bhms/business/core/user/stores/usercache"
 	"github.com/nhaancs/bhms/business/core/user/stores/userdb"
-	db "github.com/nhaancs/bhms/business/data/dbsql/pq"
 	"github.com/nhaancs/bhms/business/web/auth"
 	"github.com/nhaancs/bhms/foundation/logger"
 	"github.com/nhaancs/bhms/foundation/web"
@@ -28,17 +26,7 @@ func Routes(app *web.App, cfg Config) {
 
 	envCore := event.NewCore(cfg.Log)
 	usrCore := user.NewCore(cfg.Log, envCore, usercache.NewStore(cfg.Log, userdb.NewStore(cfg.Log, cfg.DB)))
-
-	authen := mid.Authenticate(cfg.Auth)
-	ruleAdmin := mid.Authorize(cfg.Auth, auth.RuleAdminOnly)
-	ruleAdminOrSubject := mid.Authorize(cfg.Auth, auth.RuleAdminOrSubject)
-	tran := mid.ExecuteInTransation(cfg.Log, db.NewBeginner(cfg.DB))
-
 	hdl := New(usrCore, cfg.Auth)
 	app.Handle(http.MethodGet, version, "/users/token/:kid", hdl.Token)
-	app.Handle(http.MethodGet, version, "/users", hdl.Query, authen, ruleAdmin)
-	app.Handle(http.MethodGet, version, "/users/:user_id", hdl.QueryByID, authen, ruleAdminOrSubject)
-	app.Handle(http.MethodPost, version, "/users", hdl.Create, authen, ruleAdmin)
-	app.Handle(http.MethodPut, version, "/users/:user_id", hdl.Update, authen, ruleAdminOrSubject, tran)
-	app.Handle(http.MethodDelete, version, "/users/:user_id", hdl.Delete, authen, ruleAdminOrSubject, tran)
+	app.Handle(http.MethodPost, version, "/users/register", hdl.Register)
 }
--- app/services/api/v1/handlers/usergrp/usergrp.go
+++ app/services/api/v1/handlers/usergrp/usergrp.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
-	"github.com/nhaancs/bhms/app/services/api/v1/paging"
 	"github.com/nhaancs/bhms/app/services/api/v1/request"
 	"github.com/nhaancs/bhms/business/core/user"
 	"github.com/nhaancs/bhms/business/data/transaction"
@@ -53,8 +52,8 @@ func (h *Handlers) executeUnderTransaction(ctx context.Context) (*Handlers, erro
 	return h, nil
 }
 
-// Create adds a new user to the system.
-func (h *Handlers) Create(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+// Register adds a new user to the system.
+func (h *Handlers) Register(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	var app RegisterDTO
 	if err := web.Decode(r, &app); err != nil {
 		return request.NewError(err, http.StatusBadRequest)
@@ -74,116 +73,6 @@ func (h *Handlers) Create(ctx context.Context, w http.ResponseWriter, r *http.Re
 	}
 
 	return web.Respond(ctx, w, toUserDTO(usr), http.StatusCreated)
-}
-
-// Update updates a user in the system.
-func (h *Handlers) Update(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	h, err := h.executeUnderTransaction(ctx)
-	if err != nil {
-		return err
-	}
-
-	var app UpdateUserDTO
-	if err := web.Decode(r, &app); err != nil {
-		return request.NewError(err, http.StatusBadRequest)
-	}
-
-	userID := auth.GetUserID(ctx)
-
-	usr, err := h.user.QueryByID(ctx, userID)
-	if err != nil {
-		switch {
-		case errors.Is(err, user.ErrNotFound):
-			return request.NewError(err, http.StatusNotFound)
-		default:
-			return fmt.Errorf("querybyid: userID[%s]: %w", userID, err)
-		}
-	}
-
-	uu, err := toUpdateUserEntity(app)
-	if err != nil {
-		return request.NewError(err, http.StatusBadRequest)
-	}
-
-	usr, err = h.user.Update(ctx, usr, uu)
-	if err != nil {
-		return fmt.Errorf("update: userID[%s] uu[%+v]: %w", userID, uu, err)
-	}
-
-	return web.Respond(ctx, w, toUserDTO(usr), http.StatusOK)
-}
-
-// Delete removes a user from the system.
-func (h *Handlers) Delete(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	userID := auth.GetUserID(ctx)
-
-	h, err := h.executeUnderTransaction(ctx)
-	if err != nil {
-		return err
-	}
-
-	usr, err := h.user.QueryByID(ctx, userID)
-	if err != nil {
-		switch {
-		case errors.Is(err, user.ErrNotFound):
-			return web.Respond(ctx, w, nil, http.StatusNoContent)
-		default:
-			return fmt.Errorf("querybyid: userID[%s]: %w", userID, err)
-		}
-	}
-
-	if err := h.user.Delete(ctx, usr); err != nil {
-		return fmt.Errorf("delete: userID[%s]: %w", userID, err)
-	}
-
-	return web.Respond(ctx, w, nil, http.StatusNoContent)
-}
-
-// Query returns a list of users with paging.
-func (h *Handlers) Query(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	page, err := paging.ParseRequest(r)
-	if err != nil {
-		return err
-	}
-
-	filter, err := parseFilter(r)
-	if err != nil {
-		return err
-	}
-
-	orderBy, err := parseOrder(r)
-	if err != nil {
-		return err
-	}
-
-	users, err := h.user.Query(ctx, filter, orderBy, page.Number, page.RowsPerPage)
-	if err != nil {
-		return fmt.Errorf("query: %w", err)
-	}
-
-	total, err := h.user.Count(ctx, filter)
-	if err != nil {
-		return fmt.Errorf("count: %w", err)
-	}
-
-	return web.Respond(ctx, w, paging.NewResponse(toUserDTOs(users), total, page.Number, page.RowsPerPage), http.StatusOK)
-}
-
-// QueryByID returns a user by its ID.
-func (h *Handlers) QueryByID(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	id := auth.GetUserID(ctx)
-
-	usr, err := h.user.QueryByID(ctx, id)
-	if err != nil {
-		switch {
-		case errors.Is(err, user.ErrNotFound):
-			return request.NewError(err, http.StatusNotFound)
-		default:
-			return fmt.Errorf("querybyid: id[%s]: %w", id, err)
-		}
-	}
-
-	return web.Respond(ctx, w, toUserDTO(usr), http.StatusOK)
 }
 
 // Token provides an API token for the authenticated user.
