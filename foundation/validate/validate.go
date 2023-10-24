45b59a6910d34e0a15f51737ef26e01803ea99ef
nhannq2 <nhanpublic@gmail.com>
2023-10-24 23:44:48 +0700

    updated validate translation

--- foundation/validate/validate.go
+++ foundation/validate/validate.go
@@ -5,10 +5,10 @@ import (
 	"reflect"
 	"strings"
 
-	"github.com/go-playground/locales/en"
+	"github.com/go-playground/locales/vi"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
-	en_translations "github.com/go-playground/validator/v10/translations/en"
+	vi_translations "github.com/go-playground/validator/v10/translations/vi"
 )
 
 // validate holds the settings and caches for validating request struct values.
@@ -24,10 +24,10 @@ func init() {
 
 	// Create a translator for english so the error messages are
 	// more human-readable than technical.
-	translator, _ = ut.New(en.New(), en.New()).GetTranslator("en")
+	translator, _ = ut.New(vi.New(), vi.New()).GetTranslator("vi")
 
 	// Register the english error messages for use.
-	en_translations.RegisterDefaultTranslations(validate, translator)
+	vi_translations.RegisterDefaultTranslations(validate, translator)
 
 	// Use JSON tag names for errors instead of Go struct names.
 	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
