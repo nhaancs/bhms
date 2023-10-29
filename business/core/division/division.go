3fcfc8f3c64b039f29dea235322dc9c3129135bd
nhannq2 <nhanpublic@gmail.com>
2023-10-29 17:34:58 +0700

    division biz: get provinces

--- business/core/division/division.go
+++ business/core/division/division.go
@@ -49,3 +49,12 @@ func (c *Core) QueryByParentID(ctx context.Context, parentID int) ([]Divison, er
 
 	return divs, err
 }
+
+func (c *Core) GetProvinces(ctx context.Context) ([]Divison, error) {
+	provinces, err := c.store.QueryLevel1s(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("get provinces: err: %w", err)
+	}
+
+	return provinces, err
+}
