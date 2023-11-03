8f236b28fc704337ae81290db0115d48b5af2732
nhannq2 <nhanpublic@gmail.com>
2023-11-03 15:02:27 +0700

    init property core

--- business/core/property/property.go
+++ business/core/property/property.go
@@ -3,6 +3,7 @@ package property
 import (
 	"context"
 	"github.com/google/uuid"
+	"github.com/nhaancs/bhms/foundation/logger"
 )
 
 // TODO: limit the number of properties can be created by a user
@@ -13,4 +14,16 @@ type Storer interface {
 	Delete(ctx context.Context, prprty Property) error
 	QueryByID(ctx context.Context, prprtyID uuid.UUID) (Property, error)
 	QueryByManagerID(ctx context.Context, managerID uuid.UUID) ([]Property, error)
+}
+
+type Core struct {
+	store Storer
+	log   *logger.Logger
+}
+
+func NewCore(log *logger.Logger, store Storer) *Core {
+	return &Core{
+		store: store,
+		log:   log,
+	}
 }
