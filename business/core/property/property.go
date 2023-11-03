397b883cf964ac4cd05a6fc49993ea8024159364
nhannq2 <nhanpublic@gmail.com>
2023-11-03 15:05:13 +0700

    property core: create

--- business/core/property/property.go
+++ business/core/property/property.go
@@ -2,8 +2,10 @@ package property
 
 import (
 	"context"
+	"fmt"
 	"github.com/google/uuid"
 	"github.com/nhaancs/bhms/foundation/logger"
+	"time"
 )
 
 // TODO: limit the number of properties can be created by a user
@@ -27,3 +29,76 @@ func NewCore(log *logger.Logger, store Storer) *Core {
 		log:   log,
 	}
 }
+
+func (c *Core) Create(ctx context.Context, e NewProperty) (Property, error) {
+	now := time.Now()
+
+	prprty := Property{
+		ID:        uuid.New(),
+		Status:    e.Status,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	if err := c.store.Create(ctx, prprty); err != nil {
+		return Property{}, fmt.Errorf("create: %w", err)
+	}
+
+	return prprty, nil
+}
+
+//func (c *Core) Update(ctx context.Context, usr User, uu UpdateUser) (User, error) {
+//	if uu.FirstName != nil {
+//		usr.FirstName = *uu.FirstName
+//	}
+//
+//	if uu.LastName != nil {
+//		usr.LastName = *uu.LastName
+//	}
+//
+//	if uu.Phone != nil {
+//		usr.Phone = *uu.Phone
+//	}
+//
+//	if uu.Roles != nil {
+//		usr.Roles = uu.Roles
+//	}
+//
+//	if uu.Password != nil {
+//		pw, err := bcrypt.GenerateFromPassword([]byte(*uu.Password), bcrypt.DefaultCost)
+//		if err != nil {
+//			return User{}, fmt.Errorf("generatefrompassword: %w", err)
+//		}
+//		usr.PasswordHash = pw
+//	}
+//
+//	if uu.Status != nil {
+//		usr.Status = *uu.Status
+//	}
+//
+//	usr.UpdatedAt = time.Now()
+//
+//	if err := c.store.Update(ctx, usr); err != nil {
+//		return User{}, fmt.Errorf("update: %w", err)
+//	}
+//
+//	return usr, nil
+//}
+
+//func (c *Core) QueryByID(ctx context.Context, userID uuid.UUID) (User, error) {
+//	user, err := c.store.QueryByID(ctx, userID)
+//	if err != nil {
+//		return User{}, fmt.Errorf("query: userID[%s]: %w", userID, err)
+//	}
+//
+//	return user, nil
+//}
+
+//func (c *Core) QueryByPhone(ctx context.Context, phone string) (User, error) {
+//	user, err := c.store.QueryByPhone(ctx, phone)
+//	if err != nil {
+//		return User{}, fmt.Errorf("query: phone[%s]: %w", phone, err)
+//	}
+//
+//	return user, nil
+//}
