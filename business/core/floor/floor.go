f5a9eb4d401b9d1dc0e530a825667ec9db61a11a
nhannq2 <nhanpublic@gmail.com>
2023-11-08 21:05:44 +0700

    added floor core

--- business/core/floor/floor.go
+++ business/core/floor/floor.go
@@ -1,7 +1,106 @@
 package floor
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/nhaancs/bhms/foundation/logger"
+	"time"
+)
 
 var (
 	ErrNotFound = errors.New("floor not found")
 )
+
+type Storer interface {
+	Create(ctx context.Context, core Floor) error
+	BatchCreate(ctx context.Context, cores []Floor) error
+	Update(ctx context.Context, core Floor) error
+	Delete(ctx context.Context, core Floor) error
+	QueryByID(ctx context.Context, id uuid.UUID) (Floor, error)
+	QueryByBlockID(ctx context.Context, blockID uuid.UUID) ([]Floor, error)
+}
+
+type Core struct {
+	store Storer
+	log   *logger.Logger
+}
+
+func NewCore(log *logger.Logger, store Storer) *Core {
+	return &Core{
+		store: store,
+		log:   log,
+	}
+}
+
+func (c *Core) Create(ctx context.Context, core NewFloor) (Floor, error) {
+	now := time.Now()
+	flr := Floor{
+		ID:         uuid.New(),
+		Name:       core.Name,
+		PropertyID: core.PropertyID,
+		BlockID:    core.BlockID,
+		CreatedAt:  now,
+		UpdatedAt:  now,
+	}
+
+	if err := c.store.Create(ctx, flr); err != nil {
+		return Floor{}, fmt.Errorf("create: %w", err)
+	}
+
+	return flr, nil
+}
+
+func (c *Core) BatchCreate(ctx context.Context, cores []NewFloor) ([]Floor, error) {
+	now := time.Now()
+	flrs := make([]Floor, len(cores))
+
+	for i := range cores {
+		flrs[i] = Floor{
+			ID:         uuid.New(),
+			Name:       cores[i].Name,
+			PropertyID: cores[i].PropertyID,
+			BlockID:    cores[i].BlockID,
+			CreatedAt:  now,
+			UpdatedAt:  now,
+		}
+	}
+
+	if err := c.store.BatchCreate(ctx, flrs); err != nil {
+		return nil, fmt.Errorf("batch create: %w", err)
+	}
+
+	return flrs, nil
+}
+
+func (c *Core) Update(ctx context.Context, o Floor, n UpdateFloor) (Floor, error) {
+	if n.Name != nil {
+		o.Name = *n.Name
+	}
+	o.UpdatedAt = time.Now()
+
+	if err := c.store.Update(ctx, o); err != nil {
+		return Floor{}, fmt.Errorf("update: %w", err)
+	}
+
+	return o, nil
+}
+
+func (c *Core) QueryByID(ctx context.Context, id uuid.UUID) (Floor, error) {
+	flr, err := c.store.QueryByID(ctx, id)
+	if err != nil {
+		return Floor{}, fmt.Errorf("query: id[%s]: %w", id, err)
+	}
+
+	return flr, nil
+}
+
+func (c *Core) QueryByBlockID(ctx context.Context, propertyID uuid.UUID) ([]Floor, error) {
+	flrs, err := c.store.QueryByBlockID(ctx, propertyID)
+	if err != nil {
+		return nil, fmt.Errorf("query: block id[%s]: %w", propertyID.String(), err)
+	}
+
+	return flrs, nil
+}
