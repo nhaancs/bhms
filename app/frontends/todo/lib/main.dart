8bdb9f91b15a5fc4570306785e078e1429e506a3
nhannq2 <nhanpublic@gmail.com>
2023-11-20 22:44:28 +0700

    added template files, refactored home

--- app/frontends/todo/lib/main.dart
+++ app/frontends/todo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,29 +11,5 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(home: MyHomePage());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Todo App"),
-      ),
-      body: const Center(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
   }
 }
