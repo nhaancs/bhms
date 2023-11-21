c0775980251766924ec385d87095b99dc5acbe2d
nhannq2 <nhanpublic@gmail.com>
2023-11-21 21:29:46 +0700

    todo home page

--- app/frontends/todo/lib/pages/home.dart
+++ app/frontends/todo/lib/pages/home.dart
@@ -1,21 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:todo/models/todo.dart';
 import 'package:todo/pages/add.dart';
+import 'package:todo/providers/todo_provider.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<Todo> todos = ref.watch(todoProvider);
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Todo App"),
       ),
-      body: const Center(),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return Slidable(
+            startActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              children: [
+                SlidableAction(
+                  onPressed: (context) => ref
+                      .watch(todoProvider.notifier)
+                      .deleteTodo(todos[index].todoId),
+                  icon: Icons.delete,
+                  backgroundColor: Colors.red,
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                )
+              ],
+            ),
+            endActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              children: [
+                SlidableAction(
+                  onPressed: (context) => ref
+                      .watch(todoProvider.notifier)
+                      .completeTodo(todos[index].todoId),
+                  icon: Icons.check,
+                  backgroundColor: Colors.green,
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                )
+              ],
+            ),
+            child: ListTile(title: Text(todos[index].content)),
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context)
