105808f098ac4ed6196e1be0b5fb929a531ec403
nhannq2 <nhanpublic@gmail.com>
2023-12-06 22:03:55 +0700

    roll dice: added background color

--- app/frontends/roll_dice/lib/main.dart
+++ app/frontends/roll_dice/lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.amber,
         body: Center(
           child: Text('Hello World!'),
         ),
