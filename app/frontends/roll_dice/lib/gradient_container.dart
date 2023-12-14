a6b0a2491fb19bb6850bc92484c2a2feb7b6fdf8
nhannq2 <nhanpublic@gmail.com>
2023-12-15 00:16:38 +0700

    images and multiple constructors

--- app/frontends/roll_dice/lib/gradient_container.dart
+++ app/frontends/roll_dice/lib/gradient_container.dart
@@ -1,12 +1,15 @@
 
 import 'package:flutter/material.dart';
-import 'package:roll_dice/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
+
+  const GradientContainer.purple({super.key})
+    : colors = const [Colors.deepPurple, Colors.indigo];
+
   final List<Color> colors;
 
   @override
@@ -19,8 +22,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello world!'),
+      child: Center(
+        child: Image.asset("assets/images/dice-1.png", width: 200,),
       ),
     );
   }
