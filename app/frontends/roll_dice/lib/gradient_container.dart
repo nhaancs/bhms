f1ef7b820893a7586a028c0e2cfc7e68ea8eb571
nhannq2 <nhanpublic@gmail.com>
2023-12-15 23:59:13 +0700

    roll dice: style btn

--- app/frontends/roll_dice/lib/gradient_container.dart
+++ app/frontends/roll_dice/lib/gradient_container.dart
@@ -25,13 +25,24 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               "assets/images/dice-1.png",
               width: 200,
             ),
+            const SizedBox(
+              height: 20,
+            ),
             TextButton(
               onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
               child: const Text("Roll Dice"),
             )
           ],
