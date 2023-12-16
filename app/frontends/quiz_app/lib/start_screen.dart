966c2e42b51f86080688ab59f2de8baac5a153f4
nhannq2 <nhanpublic@gmail.com>
2023-12-16 23:54:30 +0700

    quiz app: finish start screen

--- app/frontends/quiz_app/lib/start_screen.dart
+++ app/frontends/quiz_app/lib/start_screen.dart
@@ -5,6 +5,36 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Start Screen");
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            "Learn Flutter the fun way!",
+            style: TextStyle(
+              color: Color.fromARGB(255, 237, 223, 252),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text("Start Quiz"),
+          )
+        ],
+      ),
+    );
   }
 }
