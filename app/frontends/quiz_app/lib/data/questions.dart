ae0a86562ba86adbb4b7905abae6008203d2409e
nhannq2 <nhanpublic@gmail.com>
2023-12-17 15:16:04 +0700

    quiz app: question model

---
+++ app/frontends/quiz_app/lib/data/questions.dart
@@ -0,0 +1,94 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    "Question 1",
+    [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4",
+    ],
+  ),
+  QuizQuestion(
+    "Question 2",
+    [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4",
+    ],
+  ),
+  QuizQuestion(
+    "Question 3",
+    [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4",
+    ],
+  ),
+  QuizQuestion(
+    "Question 4",
+    [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4",
+    ],
+  ),
+  QuizQuestion(
+    "Question 5",
+    [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4",
+    ],
+  ),
+  QuizQuestion(
+    "Question 6",
+    [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4",
+    ],
+  ),
+  QuizQuestion(
+    "Question 7",
+    [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4",
+    ],
+  ),
+  QuizQuestion(
+    "Question 8",
+    [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4",
+    ],
+  ),
+  QuizQuestion(
+    "Question 9",
+    [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4",
+    ],
+  ),
+  QuizQuestion(
+    "Question 10",
+    [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4",
+    ],
+  ),
+];
