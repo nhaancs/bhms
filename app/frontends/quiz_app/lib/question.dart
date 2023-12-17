3ee972560d4ffba64f582fdb8d0353df0d4758d5
nhannq2 <nhanpublic@gmail.com>
2023-12-17 11:28:15 +0700

    quiz app: refactored

---
+++ app/frontends/quiz_app/lib/question.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({super.key});
+
+  @override
+  State<QuestionScreen> createState() {
+    return _QuestionScreenState();
+  }
+}
+
+class _QuestionScreenState extends State<QuestionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("QuestionScreen");
+  }
+}
