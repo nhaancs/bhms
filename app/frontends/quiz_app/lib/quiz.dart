2c99b8257d0779752648676b8eca86e2f038d956
nhannq2 <nhanpublic@gmail.com>
2023-12-18 16:28:04 +0700

    quiz app: avoid array index out of range error

--- app/frontends/quiz_app/lib/quiz.dart
+++ app/frontends/quiz_app/lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/question_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
@@ -13,10 +14,17 @@ class Quiz extends StatefulWidget {
 
 class _QuizState extends State<Quiz> {
   var activeScreen = 'start-screen';
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length >= questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start-screen';
+      });
+    }
   }
 
   void switchScreen() {
