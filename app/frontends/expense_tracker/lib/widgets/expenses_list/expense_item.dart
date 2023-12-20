90c03e968c58da88b0d0548278b1501fe76cbb4c
nhannq2 <nhanpublic@gmail.com>
2023-12-20 23:08:01 +0700

    expense tracker: refactored

---
+++ app/frontends/expense_tracker/lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,36 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Text(expense.title),
+            SizedBox(height: 4),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                Spacer(),
+                Row(
+                  children: [
+                    Icon(Icons.alarm),
+                    SizedBox(width: 8),
+                    Text(expense.date.toString()),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- app/frontends/expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ app/frontends/expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/cupertino.dart';
 
 class ExpensesList extends StatelessWidget {
@@ -11,7 +12,7 @@ class ExpensesList extends StatelessWidget {
     return Expanded(
       child: ListView.builder(
         itemCount: expenses.length,
-        itemBuilder: (ctx, index) => Text(expenses[index].title),
+        itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
       ),
     );
   }
