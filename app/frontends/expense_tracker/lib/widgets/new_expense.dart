e510e69a1102f1f3b13cdbb54b0f7bebf9da5a94
nhannq2 <nhanpublic@gmail.com>
2023-12-21 20:47:34 +0700

    expense tracker: handling user input 2

--- app/frontends/expense_tracker/lib/widgets/new_expense.dart
+++ app/frontends/expense_tracker/lib/widgets/new_expense.dart
@@ -10,19 +10,33 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _enteredTitle = '';
+
+  void _saveTitleInput(String value) {
+    _enteredTitle = value;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            onChanged: _saveTitleInput,
             maxLength: 50,
             keyboardType: TextInputType.text,
-            decoration: InputDecoration(label: Text("Title")),
+            decoration: const InputDecoration(label: Text("Title")),
           ),
           Row(
-            children: [],
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_enteredTitle);
+                },
+                child: const Text("Save Expense"),
+              )
+            ],
           ),
         ],
       ),
