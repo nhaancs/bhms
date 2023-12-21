c31f3e5d05aef1bb1b486ae28d7b3a4014ebaa9f
nhannq2 <nhanpublic@gmail.com>
2023-12-21 23:25:32 +0700

    expense tracker: finish validation

--- app/frontends/expense_tracker/lib/widgets/new_expense.dart
+++ app/frontends/expense_tracker/lib/widgets/new_expense.dart
@@ -42,7 +42,27 @@ class _NewExpenseState extends State<NewExpense> {
   void _submitExpenseData() {
     final enteredAmount = double.tryParse(_amountController.text);
     final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
-    if (_titleController.text.trim().isEmpty || amountIsInvalid) {}
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text("Invalid input"),
+          content: const Text(
+              "Please make sure a valid title, amount, date and category was entered."),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Okay"),
+            )
+          ],
+        ),
+      );
+      return;
+    }
   }
 
   @override
