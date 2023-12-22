31f6267af805af03f7afbda35642fa5ca48c96c8
nhannq2 <nhanpublic@gmail.com>
2023-12-22 08:06:00 +0700

    expense tracker: fullscreen modal

--- app/frontends/expense_tracker/lib/widgets/new_expense.dart
+++ app/frontends/expense_tracker/lib/widgets/new_expense.dart
@@ -74,6 +74,8 @@ class _NewExpenseState extends State<NewExpense> {
         category: _selectedCategory,
       ),
     );
+
+    Navigator.pop(context);
   }
 
   @override
@@ -86,7 +88,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
