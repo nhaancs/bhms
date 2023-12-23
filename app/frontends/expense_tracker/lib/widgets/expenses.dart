65574f80c34b1801184079b9f5ae9895c41d6334
nhannq2 <nhanpublic@gmail.com>
2023-12-23 23:57:28 +0700

    expense tracker: updated ui based on available width

--- app/frontends/expense_tracker/lib/widgets/expenses.dart
+++ app/frontends/expense_tracker/lib/widgets/expenses.dart
@@ -70,6 +70,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text("Not expenses found. Start adding some!"),
     );
@@ -91,12 +93,19 @@ class _ExpensesState extends State<Expenses> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          mainContent,
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                mainContent,
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(child: Chart(expenses: _registeredExpenses)),
+                mainContent,
+              ],
+            ),
     );
   }
 }
