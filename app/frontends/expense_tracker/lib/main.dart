40d97fe495fbf710bf66c9d8289912233d724a14
nhannq2 <nhanpublic@gmail.com>
2023-12-23 23:47:10 +0700

    expense tracker: locked device orientation

--- app/frontends/expense_tracker/lib/main.dart
+++ app/frontends/expense_tracker/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:expense_tracker/widgets/expenses.dart';
-import 'package:flutter/materialfromSeed(
+import 'package:flutter/material.dart';
+
+var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
