27f7b21ab674b4322eba28dcd446d1c9b398b814
nhannq2 <nhanpublic@gmail.com>
2023-12-23 23:44:49 +0700

    expense tracker: locked device orientation

--- app/frontends/expense_tracker/lib/main.dart
+++ app/frontends/expense_tracker/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
@@ -10,7 +11,13 @@ var kDarkColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 5, 99, 125),
 );
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
